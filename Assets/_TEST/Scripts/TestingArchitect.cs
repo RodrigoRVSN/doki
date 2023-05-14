cd826521a3b456db1941fb97713118326e2ad043
rodrigorvsn <rodrigovictor81@gmail.com>
2023-05-14 14:49:18 -0300

    feat: add initial text builder config

---
+++ Assets/_TEST/Scripts/TestingArchitect.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TESTING
+{
+    public class TestingArchitect : MonoBehaviour
+    {
+        DialogueSystem ds;
+        TextArchitect architect;
+
+        string[] lines = new string[5]
+        {
+            "Hello World!",
+            "This is a test.",
+            "This is a test of the emergency broadcast system.",
+            "This is only a test.",
+            "Yes, this is only a test.",
+        };
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ds = DialogueSystem.instance;
+            architect = new TextArchitect(ds.dialogueContainer.dialogueText);
+            architect.buildMethod = TextArchitect.BuildMethod.typewriter;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            string longLine = "This is a test of the emergency broadcast system. This is only a test. Yes, this is only a test okay. A very ang big long good line to be very long.";
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if (architect.isBuilding)
+                {
+                    if (!architect.hurryUp)
+                    {
+                        architect.hurryUp = true;
+                    }
+                    else
+                    {
+                        architect.ForceComplete();
+                    }
+                }
+                else
+                    // architect.Build(lines[Random.Range(0, lines.Length)])
+                    architect.Build(longLine);
+            } else if (Input.GetKeyDown(KeyCode.A))
+            {
+                architect.Append(longLine);
+                // architect.Append(lines[Random.Range(0, lines.Length)]);
+            }
+        }
+    }
+
+}
